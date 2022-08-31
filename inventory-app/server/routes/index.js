578308f5281be3e0f4010aa7e8f61fa38e9a26bf
Ingrit McKiernan <david.mckiernan@barclays.com>
2022-08-31 11:18:53 +0100

    new-get-routes

--- inventory-app/server/routes/index.js
+++ inventory-app/server/routes/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const router = express.Router();
+const routerItem = express.Router();
 
 //different model routers
 router.use('/items', require('./items'));
 router.use('/users', require('./users'))
 
-module.exports = router;
+module.exports = routerItem;
