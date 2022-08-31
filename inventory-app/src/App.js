6e4df4c8d91c98a3025b0ba3acb1243ecccca04b
James Tierney <james.tierney384@gmail.com>
2022-08-31 10:26:13 +0100

    initial commit

---
+++ inventory-app/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <main >	
+        <h1>The Engineering org</h1>
+        <h2>Inventory Items</h2>
+            <div className = "card">
+            <div className = "card-header">This Could be the Name of the item</div>
+            <div className = "card-body">Maybe a description of the item</div>
+            <div className = "card-footer">
+            <button className = "btn">one or 2 buttons?</button>
+            <button className = "btn2">one or 2 buttons?</button>     
+            </div>    
+          </div>
+      </main>
+  );
+}
+
+export default App;
